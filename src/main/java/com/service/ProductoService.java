42508429f182777ed39483fdd70f4bfcd84a48ee
Stif7 <147210817+Stif7@users.noreply.github.com>
2024-04-21 22:53:39 -0600

    Update ProductoService.java

--- src/main/java/com/service/ProductoService.java
+++ src/main/java/com/service/ProductoService.java
@@ -26,5 +26,13 @@ public interface ProductoService{
     // Se elimina el categoria que tiene el id pasado por parámetro  
     public void delete(Producto producto);
 
-
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    
+    
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    
+     public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
+
+
